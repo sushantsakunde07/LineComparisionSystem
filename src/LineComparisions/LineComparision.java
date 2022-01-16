ae65ae02d17d90d13718f3157dba6f6ce1e9e128
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-16 20:48:20 +0530

    Added: Length of line

--- src/LineComparisions/LineComparision.java
+++ src/LineComparisions/LineComparision.java
@@ -1,9 +1,26 @@
 package LineComparisions;
 
+import java.util.Scanner;
+
 public class LineComparision {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparision");
+        double x1 = 0;
+        double x2 = 0;
+        double y1 = 0;
+        double y2 = 0;
+        double length;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first line point coordinate x1");
+        x1 = scan.nextInt();
+        System.out.println("Enter first line point coordinate y1");
+        y1 = scan.nextInt();
+        System.out.println("Enter second line point coordinate x2");
+        x2 = scan.nextInt();
+        System.out.println("Enter second line point coordinate y2");
+        y2 = scan.nextInt();
+        scan.close();
+        length = (Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2))));
+        System.out.println("Length of line: " + length);
     }
 }
-
 
