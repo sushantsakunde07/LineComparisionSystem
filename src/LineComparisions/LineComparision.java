ba456e0165d1be149e0ad6b8f9ff22512f513cf3
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-16 20:54:50 +0530

    Added: Length of line equality

--- src/LineComparisions/LineComparision.java
+++ src/LineComparisions/LineComparision.java
@@ -8,19 +8,35 @@ public class LineComparision {
         double x2 = 0;
         double y1 = 0;
         double y2 = 0;
+        double x3 = 0;
+        double x4 = 0;
+        double y3 = 0;
+        double y4 = 0;
         double length;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter first line point coordinate x1");
+        System.out.println("Enter first point coordinate x1");
         x1 = scan.nextInt();
-        System.out.println("Enter first line point coordinate y1");
+        System.out.println("Enter first point coordinate y1");
         y1 = scan.nextInt();
-        System.out.println("Enter second line point coordinate x2");
+        System.out.println("Enter second point coordinate x2");
         x2 = scan.nextInt();
-        System.out.println("Enter second line point coordinate y2");
+        System.out.println("Enter second point coordinate y2");
         y2 = scan.nextInt();
+        System.out.println("Enter third point coordinate x3");
+        x3 = scan.nextInt();
+        System.out.println("Enter third point coordinate y3");
+        y3 = scan.nextInt();
+        System.out.println("Enter fourth point coordinate x4");
+        x4 = scan.nextInt();
+        System.out.println("Enter fourth point coordinate y4");
+        y4 = scan.nextInt();
         scan.close();
-        length = (Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2))));
-        System.out.println("Length of line: " + length);
+        Double length1 = (Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2))));
+        Double length2 = (Math.sqrt(Math.pow(x4 - x3, 2) + (Math.pow(y4 - y3, 2))));
+        if (length1.equals(length2)) {
+            System.out.println("Length of both lines are equal");
+        } else {
+            System.out.println("length of first line is not equal second line");
+        }
     }
 }
-
